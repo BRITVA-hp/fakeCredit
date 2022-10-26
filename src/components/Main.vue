2b658b6e891748bffebc017ae20fe7663a87c896
BRITVA-hp <borbritvahp@gmail.com>
2022-10-26 15:30:58 +0400

    start main

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="py-10 px-5 section min-h-screen m-auto max-w-8xl bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div class="container p-4 rounded-xl shadow-md m-auto bg-gray-50">
+      <h1 class="text-center font-bold text-lg lg:text-2xl text-slate-600">Проверка скорингового балла ЧЕРЕЗ БКИ</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
